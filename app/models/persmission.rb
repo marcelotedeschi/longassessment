555b604e212ea3513a8813d7994efece238cb086
Marcelo Tedeschi <marcelotedeschi@hotmail.com>
2016-05-23 16:40:25 -0300

    models and references created

--- app/models/persmission.rb
+++
@@ -1,5 +0,0 @@
-class Persmission < ActiveRecord::Base
-
-  belongs_to :role
-  has_and_belongs_to_many :users
-end
--- db/migrate/20160523181121_create_persmissions.rb
+++
@@ -1,9 +0,0 @@
-class CreatePersmissions < ActiveRecord::Migration
-  def change
-    create_table :persmissions do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
